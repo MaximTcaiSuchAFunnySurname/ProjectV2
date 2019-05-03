d48066aff6f0ced941fdefdbaff498f673ef00bd
maxim <max@mail.ru>
2019-05-03 18:32:26 +0300

    TodoFragment Addition

--- app/src/main/java/com/example/maxim/protov40/fragments/TodoFragment.java
+++ app/src/main/java/com/example/maxim/protov40/fragments/TodoFragment.java
@@ -10,6 +10,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.maxim.protov40.R;
+import com.example.maxim.protov40.util.Session;
 
 import org.w3c.dom.Text;
 
@@ -18,20 +19,41 @@ public class TodoFragment extends Fragment {
     private TextView todoText;
     private TextView todoData;
 
-    public TodoFragment(){}
+    public TodoFragment() {
+    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.todo_layout, container, false);
         todoName = (TextView) view.findViewById(R.id.todo_name);
         todoText = (TextView) view.findViewById(R.id.todo_text);
         todoData = (TextView) view.findViewById(R.id.todo_data);
+        todoName.setText(getTodoInfo("name"));
+        todoText.setText(getTodoInfo("text"));
+        todoData.setText(getTodoInfo("data"));
         return view;
+    }
+
+    public String getTodoInfo(String key) {
+        switch (key.toString()) {
+            case "name":
+                return Session.getINSTANCE().getUser().getFolders()
+                        .get(getArguments().getInt("folderIndex")).getTodos()
+                        .get(getArguments().getInt("todoIndex")).getName();
+            case "text": return Session.getINSTANCE().getUser().getFolders()
+                    .get(getArguments().getInt("folderIndex")).getTodos()
+                    .get(getArguments().getInt("todoIndex")).getText();
+            case "data":
+                return Session.getINSTANCE().getUser().getFolders()
+                        .get(getArguments().getInt("folderIndex")).getTodos()
+                        .get(getArguments().getInt("todoIndex")).getData();
+        }
+        return "";
     }
 
 
